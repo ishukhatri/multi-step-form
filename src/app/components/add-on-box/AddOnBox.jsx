0b70137f75574852e2eaa9ffa8b80ae78e4b259f
ishu <ishukhatri@users.noreply.github.com>
2023-07-21 07:15:58 +0530

    some refcatoring and finishing up page

--- src/app/components/add-on-box/AddOnBox.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AddOnBox = () => {
-  return <div>AddOnBox</div>;
-};
-
-export default AddOnBox;
---
+++ src/app/components/finishing-up/FinishingUp.stories.jsx
@@ -0,0 +1,8 @@
+import FinishingUp from "./FinishingUp";
+
+export default {
+  title: "Components/FinishingUp",
+  component: FinishingUp,
+};
+
+export const FinishingUpStory = () => <FinishingUp />;
