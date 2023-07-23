2515eb9e6fc1ee6e49b7028f482722b6dbc350ce
ishu <ishukhatri@users.noreply.github.com>
2023-07-23 17:24:51 +0530

    save point

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,6 +9,33 @@ const preview = {
         date: /Date$/,
       },
     },
+    viewport: {
+      defaultViewport: "mobile", // Set the default viewport to responsive
+      viewports: {
+        // Define specific viewports with desired widths
+        desktop: {
+          name: "Desktop",
+          styles: {
+            width: "1440px", // Set the desired width for the desktop viewport
+            height: "1205px", // Set the desired height
+          },
+        },
+        tablet: {
+          name: "Tablet",
+          styles: {
+            width: "768px", // Set the desired width for the tablet viewport
+            height: "1197px", // Set the desired height
+          },
+        },
+        mobile: {
+          name: "Mobile",
+          styles: {
+            width: "375px", // Set the desired width for the mobile viewport
+            height: "1065px", // Set the desired height
+          },
+        },
+      },
+    },
   },
 };
 
