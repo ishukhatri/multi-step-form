b328a1449b99d4163092d72b40e4c1341f876895
ishu <ishukhatri@users.noreply.github.com>
2023-08-23 07:35:30 +0530

    form 3 refactor

--- src/app/components/forms/form3-addons/Form3-Addons.jsx
+++ src/app/components/forms/form3-addons/Form3-Addons.jsx
@@ -1,9 +1,43 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import AddonBox from "./addon-box/AddonBox";
 import FormLayout from "../FormLayout";
 
-const AddonsForm = ({ billingCycle }) => {
+const AddonBox = ({ addon, isMonthly }) => {
+  return (
+    <div className="flex flex-row grow items-center">
+      <div className="flex flex-col flex-1 gap-1 ">
+        <p className="text-sky-950 text-sm font-medium">{addon.displayName}</p>
+        <p className="text-gray-400 text-xs font-normal leading-tight">
+          {addon.desc}
+        </p>
+      </div>
+      <p className="text-right text-indigo-600 text-xs font-normal leading-tight">
+        +$
+        {isMonthly ? `${addon.monthly}/mo` : `${addon.yearly}/yr`}
+      </p>
+    </div>
+  );
+};
+
+const AddonLabel = ({ addon, register, isMonthly }) => {
+  return (
+    <label
+      key={addon.name}
+      className="flex flex-row items-center px-4 py-3 gap-4 bg-white rounded-lg border border-gray-300"
+    >
+      <input
+        type="checkbox"
+        name={`${addon.name}`} // Use a unique name for each checkbox
+        value="true" // Set a value for the checkbox
+        {...register(`${addon.name}`)} // Register the checkbox with useForm
+        className="h-5 w-5 checked:text-red-600 default:text-red-600 border-gray-300 rounded"
+      />
+      <AddonBox addon={addon} isMonthly={isMonthly} />
+    </label>
+  );
+};
+
+const AddonsForm = ({ isMonthly = false }) => {
   const {
     handleSubmit,
     control,
@@ -49,19 +83,12 @@ const AddonsForm = ({ billingCycle }) => {
       <form>
         <div className="flex flex-col gap-3">
           {addons.map((addon) => (
-            <label
+            <AddonLabel
               key={addon.name}
-              className="flex flex-row items-center px-4 py-3 gap-4 bg-white rounded-lg border border-gray-300"
-            >
-              <input
-                type="checkbox"
-                name={`${addon.name}`} // Use a unique name for each checkbox
-                value="true" // Set a value for the checkbox
-                {...register(`${addon.name}`)} // Register the checkbox with useForm
-                className="h-5 w-5 checked:text-red-600 default:text-red-600 border-gray-300 rounded"
-              />
-              <AddonBox addon={addon} billingCycle={billingCycle}></AddonBox>
-            </label>
+              addon={addon}
+              register={register}
+              isMonthly={isMonthly}
+            />
           ))}
         </div>
       </form>
